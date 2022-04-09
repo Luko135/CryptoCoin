bd17bb1ead0a04d48ab0788421324697274bd9ea
Luko <lukoszek.kontakt@onet.pl>
2022-04-09 15:48:54 +0200

    add first background

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,54 @@
+import Background from 'components/helpers/Background';
+import React from 'react';
+import styled from 'styled-components';
+// import background from 'assets/background1.svg'
+
+
+const Wrapper = styled.div`
+height: 100vh;
+`;
+const Header= styled.header`
+  width: 100%;
+  height: 70px;
+display: flex;
+justify-content:space-between ;
+align-items:center ;
+padding: 0px 20px  ;
+`
+const Navigation = styled.nav`
+height: 100%;
+display: flex;
+justify-content:space-around ;
+align-items:center ;
+width: 50% ;
+`
+const NavigationList = styled.ul`
+display: flex ;
+flex: 1 ;
+justify-content: space-between ;
+align-items: center ;
+list-style: none ;
+`
+
 function Root() {
-  return <></>;
+  return (
+    <Wrapper>
+      <Background/>
+      <Header>
+        <img src="#" alt="logo" />
+        <Navigation>
+          <NavigationList>
+            <li>All Coins</li>
+            <li>Best & Worst</li>
+            <li>NFT</li>
+            <li>New Coins</li>
+            <button>Register</button>
+            <button>LogIn</button>
+          </NavigationList>
+        </Navigation>
+      </Header>
+    </Wrapper>
+  );
 }
 
 export default Root;
---
+++ src/components/helpers/Background.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import bottom from 'assets/bottom1.svg';
+import left from 'assets/left1.svg';
+import top from 'assets/top1.svg';
+import right from 'assets/right1.svg';
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  z-index: -1;
+`;
+const Top = styled.img`
+  position: absolute;
+  left: 548px;
+`;
+const Left = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+`;
+const Right = styled.img`
+  position: absolute;
+  right: 0;
+`;
+const Bottom = styled.img`
+  position: absolute;
+  bottom: 0px;
+  left: 763px;
+`;
+
+const Background = () => {
+  return (
+    <Wrapper>
+      <Top src={top} alt="" />
+      <Left src={left} alt="" />
+      <Right src={right} alt="" />
+      <Bottom src={bottom} alt="" />
+    </Wrapper>
+  );
+};
+
+export default Background;
